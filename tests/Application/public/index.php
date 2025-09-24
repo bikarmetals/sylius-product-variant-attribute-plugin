95c15e17aba5bac3560a6d262e036e647c417947
Nail Gafarov <nail.gafarov@bikar.com>
2025-09-24 13:24:22 +0200

    fix(bundle): :ambulance: update `tests/Application` up to 1.10
    - delete undefined `SyliusAdminApiBundle`, `ConsoleColorDiffBundle`
    - uninstall `friendsofsymfony/oauth-server-bundle`

--- tests/Application/public/index.php
+++ tests/Application/public/index.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-use Tests\Umanit\SyliusProductVariantAttributePlugin\Application\Kernel;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
+use Tests\Umanit\SyliusProductVariantAttributePlugin\Application\Kernel;
 
 require dirname(__DIR__) . '/config/bootstrap.php';
 
